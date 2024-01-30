11316b14be00f8c72489c5a197f965a7cf25707c
Shrijal Acharya <shrijal.acharya@gmail.com>
2024-01-30 22:59:17 +0545

    fix: Mistake in the keybinding in the alpha.json for toggle file explorer

--- nvim-primary-config/nvim/lua/plugins/alpha.lua
+++ nvim-primary-config/nvim/lua/plugins/alpha.lua
@@ -30,7 +30,7 @@ return {
     -- Set menu
     dashboard.section.buttons.val = {
       dashboard.button("e", "  > New File", "<cmd>ene<CR>"),
-      dashboard.button("SPC ee", "  > Toggle file explorer", "<cmd>Neotree filesystem reveal left<CR>"),
+      dashboard.button("SPC b", "  > Toggle file explorer", "<cmd>Neotree filesystem reveal left<CR>"),
       dashboard.button("SPC ff", "󰱼  > Find File", "<cmd>Telescope find_files<CR>"),
       dashboard.button("SPC fs", "  > Find Word", "<cmd>Telescope live_grep<CR>"),
       dashboard.button("SPC wr", "󰁯  > Restore Session For Current Directory", "<cmd>SessionRestore<CR>"),
