7efe5b28564e8d8bab9907c6b8f62fd590ddd847
Shrijal Acharya <shrijal.acharya@gmail.com>
2024-02-23 14:51:09 +0545

    refact(IGNORE): Place the configs in custom folder

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,42 +1,4 @@
--- Import all the custom keymaps, groups and custom vim options.
--- Always load the keymappings before plugins, as they often rely on them.
-require("core.options")
-require("core.keymaps")
+require("shricodev.core")
 
--- Load the misc settings
-require("core.misc")
-
--- This is for the lazy.nvim setup
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
--- EO lazy.nvim setup
-
-require("lazy").setup({
-  { import = "plugins" },
-  { import = "plugins.lsp" },
-}, {
-  install = {
-    colorscheme = { "catppuccin", "default" },
-  },
-  checker = {
-    enabled = true,
-    notify = false,
-  },
-  change_detection = {
-    notify = false,
-    enabled = true,
-  },
-  ui = {
-    border = "rounded",
-  },
-})
+-- Require the lazy plugin manager.
+require("shricodev.lazy")
---
+++ nvim/.config/nvim/lua/shricodev/core/init.lua
@@ -0,0 +1,6 @@
+-- Require options file first because we have leaders set here
+-- Require the custom keymaps
+require("shricodev.core.options")
+require("shricodev.core.keymaps")
+
+require("shricodev.core.misc")
