d3e9563c213721b82566e100211131e019433e24
Shrijal Acharya <shrijal.acharya@gmail.com>
2024-03-01 15:30:57 +0545

    feat: Add configs for neofetch and customise wezT

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,56 +1,68 @@
 local wezterm = require("wezterm")
 
 wezterm.on("gui-startup", function(cmd)
-	local _, _, window = wezterm.mux.spawn_window(cmd or {})
-	window:gui_window():toggle_fullscreen()
+  local _, _, window = wezterm.mux.spawn_window(cmd or {})
+  window:gui_window():toggle_fullscreen()
 end)
 
 return {
-	color_scheme = "Catppuccin Mocha",
-	enable_tab_bar = false,
-	font = wezterm.font("GeistMono Nerd Font", { weight = "Bold" }),
-	hide_mouse_cursor_when_typing = true,
-	hide_tab_bar_if_only_one_tab = true,
-	line_height = 1.2,
-	font_size = 12.6,
-	disable_default_key_bindings = true,
-	cursor_blink_rate = 0,
-	window_close_confirmation = "AlwaysPrompt",
-	scrollback_lines = 10000,
-	show_update_window = true,
-	window_padding = {
-		left = 0,
-		right = 0,
-		top = 0,
-		bottom = 0,
-	},
-
-	window_background_opacity = 0.9,
-	inactive_pane_hsb = {
-		brightness = 0.5,
-		saturation = 0.24,
-	},
-	window_decorations = "RESIZE",
-	keys = {
-		{ action = wezterm.action.ActivateCommandPalette, mods = "CTRL|SHIFT", key = "P" },
-		{ action = wezterm.action.CopyTo("Clipboard"), mods = "CTRL|SHIFT", key = "C" },
-		{ action = wezterm.action.DecreaseFontSize, mods = "CTRL", key = "-" },
-		{ action = wezterm.action.IncreaseFontSize, mods = "CTRL", key = "=" },
-		{ action = wezterm.action.Nop, mods = "ALT", key = "Enter" },
-		{ action = wezterm.action.PasteFrom("Clipboard"), mods = "CTRL|SHIFT", key = "V" },
-		{ action = wezterm.action.ResetFontSize, mods = "CTRL", key = "0" },
-		{
-			action = wezterm.action.ToggleFullScreen,
-			mods = "CTRL",
-			key = "f",
-		},
-	},
-	mouse_bindings = {
-		-- Ctrl-click will open the link under the mouse cursor
-		{
-			event = { Up = { streak = 1, button = "Left" } },
-			mods = "CTRL",
-			action = wezterm.action.OpenLinkAtMouseCursor,
-		},
-	},
+  unicode_version = 15,
+  color_scheme = "Catppuccin Mocha",
+  adjust_window_size_when_changing_font_size = false,
+  audible_bell = "Disabled",
+  enable_tab_bar = false,
+  font = wezterm.font({
+    family = "GeistMono Nerd Font",
+    weight = "Bold",
+    stretch = "Normal",
+    style = "Normal",
+    harfbuzz_features = { "cv29", "cv30", "ss01", "ss03", "ss06", "ss07", "ss09" },
+  }),
+  hide_mouse_cursor_when_typing = true,
+  hide_tab_bar_if_only_one_tab = true,
+  line_height = 1.2,
+  font_size = 12.6,
+  exit_behavior = "Close",
+  disable_default_key_bindings = true,
+  cursor_blink_rate = 0,
+  window_close_confirmation = "AlwaysPrompt",
+  scrollback_lines = 10000,
+  use_dead_keys = false,
+  -- Make the cursor red if prev command failed, green if succeeded
+  force_reverse_video_cursor = true,
+  show_update_window = true,
+  window_padding = {
+    left = 0,
+    right = 0,
+    top = 0,
+    bottom = 0,
+  },
+  window_background_opacity = 1,
+  inactive_pane_hsb = {
+    brightness = 0.5,
+    saturation = 0.24,
+  },
+  window_decorations = "RESIZE",
+  keys = {
+    { action = wezterm.action.ActivateCommandPalette, mods = "CTRL|SHIFT", key = "P" },
+    { action = wezterm.action.CopyTo("Clipboard"), mods = "CTRL|SHIFT", key = "C" },
+    { action = wezterm.action.DecreaseFontSize, mods = "CTRL", key = "-" },
+    { action = wezterm.action.IncreaseFontSize, mods = "CTRL", key = "=" },
+    { action = wezterm.action.Nop, mods = "ALT", key = "Enter" },
+    { action = wezterm.action.PasteFrom("Clipboard"), mods = "CTRL|SHIFT", key = "V" },
+    { action = wezterm.action.ResetFontSize, mods = "CTRL", key = "0" },
+    {
+      action = wezterm.action.ToggleFullScreen,
+      mods = "CTRL",
+      key = "f",
+    },
+  },
+  mouse_bindings = {
+    -- Ctrl-click will open the link under the mouse cursor
+    {
+      event = { Up = { streak = 1, button = "Left" } },
+      mods = "CTRL",
+      action = wezterm.action.OpenLinkAtMouseCursor,
+    },
+  },
 }
