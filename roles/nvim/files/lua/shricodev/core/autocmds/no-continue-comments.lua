4745e13acbd1d202fb6b95f7366d921f38e684fc
Shrijal Acharya <shrijal.acharya@gmail.com>
2025-11-12 22:25:04 +0545

    feat(nvim): don't continue comments in the next line

---
+++ roles/nvim/files/lua/shricodev/core/autocmds/no-continue-comments.lua
@@ -0,0 +1,7 @@
+-- no auto continue comments on new line
+vim.api.nvim_create_autocmd('FileType', {
+  group = vim.api.nvim_create_augroup('no_auto_comment', {}),
+  callback = function()
+    vim.opt_local.formatoptions:remove { 'c', 'r', 'o' }
+  end,
+})
